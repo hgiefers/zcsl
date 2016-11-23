1939a5f4d3f6c5f78c24e157a22e065df03d7512
Heiner Giefers <hgi@zurich.ibm.com>
2016-11-23 18:38:25 +0100

    Mod run.sh

--- sw/zcsl-fft/run.sh
+++ sw/zcsl-fft/run.sh
@@ -2,7 +2,10 @@
 
 make
 vvp -m /root/zcsl/sw/pslse/afu_driver/src/libvpi.so /root/zcsl/sim/zcsl/zcsl_isim &
+sleep 7
 cd /root/zcsl/sw/pslse/pslse/
 ./pslse &
+sleep 3
 cd /root/zcsl/sw/zcsl-fft
 ./zcsl-fft-sim
+killall vvp pslse
