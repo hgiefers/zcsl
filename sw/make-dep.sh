f3d790af20b926538e120d9fc6325c66c0dc6130
Fabian Schuiki <fschuiki@iis.ee.thz.ch>
2017-03-16 10:53:14 +0100

    Fix dependency fetch and update
    The portion of `sw/make-deps.sh` that tries to update the pslse and
    libcxl dependencies is currently broken. Adjust the script to properly
    fetch and checkout, or fast-forward pull new changes. Also add a `cd`
    line at the top, allowing the script to be called from an arbitrary
    working directory.

--- sw/make-dep.sh
+++ sw/make-dep.sh
@@ -1,28 +1,35 @@
-#!/bin/bash
+#!/bin/bash -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Checkout Power Service Layer Simulation Engine (PSLSE). Contains the simulation version of libcxl.c.
 if [ ! -d "pslse" ]; then
-  git clone https://github.com/ibm-capi/pslse.git
+	git clone https://github.com/ibm-capi/pslse.git
 	cd pslse
-  git checkout tags/v2.0
+	git checkout tags/v2.0
 	cd ..
 else
-	git pull pslse
+	cd pslse
+	git fetch origin
+	git checkout tags/v2.0
+	cd ..
 fi
 
 # Checkout HW libcxl
 if [ ! -d "libcxl" ]; then
-  git clone https://github.com/ibm-capi/libcxl.git
+	git clone https://github.com/ibm-capi/libcxl.git
 else
-	git pull libcxl
+	cd libcxl
+	git pull --ff-only
+	cd ..
 fi
 
 if uname -a | grep "x86" 1> /dev/null
-then 
-	make -C ./pslse/afu_driver/src
-	make -C ./pslse/pslse
-	make -C ./pslse/libcxl
-	make -C ./pslse/debug
+then
+	make -B -C pslse/afu_driver/src
+	make -B -C pslse/pslse
+	make -B -C pslse/libcxl
+	make -B -C pslse/debug
 elif uname -a | grep ppc64 1> /dev/null
 then
 	make -C ./libcxl
